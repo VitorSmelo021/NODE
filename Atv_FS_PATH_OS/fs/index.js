b88f591a3414e9ffb9d6f4f3b4ff876ef3c16777
Vitor Samuel <108810741+VitorSmelo021@users.noreply.github.com>
2023-07-07 10:08:42 -0300

    Add files via upload

---
+++ Atv_FS_PATH_OS/fs/index.js
@@ -0,0 +1,41 @@
+
+// Atividade 01
+
+const fs = require('fs');
+
+
+// fs.readFile('atv-1.txt','utf-8', (err,data) => {
+//     if(err){
+//         console.error(err);
+//         return
+//     }
+//     console.log(data)
+// })
+
+
+
+// Atividade 02
+
+// fs.writeFile('atv-2.txt','att2', (err,data) => {
+//     if(err){
+//         console.error(err);
+//         return
+//     }
+//     console.log(data)
+// })
+
+
+// Atividade 03
+
+// const nameNewLine = 'nfjht45' + ',\r\n'
+
+//         fs.appendFile('atv-3.txt', nameNewLine, function(){
+//             res.writeHead(302, {
+//                 Location: '/',
+//             })  
+//             return res.end()  
+            
+//         })
+        
+
+        
